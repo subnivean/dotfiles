f479ec4e05f0fe10c6175369ca70f92aec697301
Mark Krautheim <mark.krautheim@clowderhill.com>
2012-11-25 23:15:02 -0500

    Adding more configs

---
+++ bash_aliases
@@ -0,0 +1,6 @@
+alias ll='ls -lA --color=always'
+alias lt='ls -tl --color=always |head -20'
+alias ltm='ls -tl |more'
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc"    # list of files/folders to symlink in homedir
+files="vimrc bashrc bash_aliases inputrc dir_functions dirstack gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
